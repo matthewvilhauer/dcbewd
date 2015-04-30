3b305e5119b2717d1eca647676145aaee54bd4ca
Matthew Vilhauer <matthewvilhauer@gmail.com>
2015-04-29 23:39:46 -0400

    added midterm

---
+++ midterm/midterm.rb
@@ -0,0 +1,26 @@
+
+# Define the setlist class
+class concert_recording
+  attr_accessor :band, :date, :venue
+
+  def initialize(band, date, venue, setlist)
+    @band = band
+    @date = date
+    @venue = venue
+    @setlist = []
+  end
+
+
+
+
+# Make a class that takes input from the user and checks if it is a string
+
+class band_search
+
+end
+
+# Make a class that takes input from the user and checks if it is a string formatted as a date
+
+class date_search
+
+end
