d432e8ff814f0f1e90e770ce70f676e691723447
Matthew Vilhauer <matthewvilhauer@gmail.com>
2015-05-05 01:29:27 -0400

    first pass at the midterm

--- midterm/midterm.rb
+++ midterm/midterm.rb
@@ -1,26 +1,79 @@
 
-# Define the setlist class
-class concert_recording
-  attr_accessor :band, :date, :venue
 
-  def initialize(band, date, venue, setlist)
-    @band = band
-    @date = date
-    @venue = venue
-    @setlist = []
+##This is a simple program that gives you a list of sustainable materials, and allows the user to pick a material, then receive back all of the different pros, cons, and things to consider when using that material
+
+require 'json'
+require 'open-uri'
+
+##First we need to pull the list of materials our users can choose from 
+def lookup_materials
+  raw_response = open('https://api.data.gov/sftool/v1/materials?api_key=dMBLd1yIH2IPbsLLSXncwX4CPaZkPm8FH0wT7Ipp')
+  response = JSON.load(raw_response)
+
+  response.map  do |material|
+    material_hash = {
+      id: material["id"],
+      name: material["name"],
+      overview: material["overview"]
+    }
   end
-
-
-
-
-# Make a class that takes input from the user and checks if it is a string
-
-class band_search
-
 end
 
-# Make a class that takes input from the user and checks if it is a string formatted as a date
-
-class date_search
-
+##Next we need a method to display the list of materials 
+def display_materials(materials)
+  materials.sort_by! { |material| material[:id] }
+ 
+  materials.reverse!
+ 
+  materials.each do |material|
+    puts "ID: #{material[:id]}"
+    puts "Name: #{material[:name]}"
+    puts "Overview: (#{material[:overview]})"
+    puts ""
+  end
 end
+
+##
+puts "!!! Welcome to the Sustainable Materials Suggestion Program !!!"
+materials = lookup_materials
+display_materials(materials)
+
+##Display the comparisons
+puts "Once you have decided the material you want to find alternatives for, enter the materials ID number below:"
+
+
+
+def lookup_comparisons chosen_material
+
+  @chosen_material = gets.chomp
+
+  ##add code here for number validation
+
+  ##Get the comparisons for the material specified
+  raw_response = open("https://api.data.gov/sftool/v1/materials/#{@chosen_material}/comparisons?api_key=dMBLd1yIH2IPbsLLSXncwX4CPaZkPm8FH0wT7Ipp")
+  response = JSON.load(raw_response)
+
+  response.map  do |comparison|
+    comparison_hash = {
+      category: comparison["category"],
+      area: comparison["area"],
+      details: comparison["details"],
+      type: comparison["type"]
+    }
+  end
+end
+
+##display the comparisons for the material specified
+def display_comparisons(comparisons)
+ 
+  comparisons.each do |comparison|
+    puts "Category: #{comparison[:category]}"
+    puts "Area: #{comparison[:area]}"
+    puts "Details: (#{comparison[:details]})"
+    puts "Type: (#{comparison[:type]})"
+    puts ""
+  end
+end
+
+comparisons = lookup_comparisons
+display_comparisons(comparisons)
